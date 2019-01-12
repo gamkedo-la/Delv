01d6878e26a55031e34edb8e7bd3bbf9781d7df6
Trent <Xmedia101@yahoo.com>
2019-01-11 19:24:14 -0500

    MainMenu scene and fixed mouse bug
    MainMenu scene practical pass
    Fixed mouse aiming bug when swapping controllers.

--- Assets/Scripts/PlayerScripts/Aimer.cs
+++ Assets/Scripts/PlayerScripts/Aimer.cs
@@ -19,7 +19,7 @@ public class Aimer : MonoBehaviour {
         ParentPlayerIndex = PC.PlayerIndex;
         Transform PlayerTF = this.transform.parent;
         PlayerGO = PlayerTF.gameObject;
-        CursorGO = GameObject.Find("Cursor");
+        CursorGO = GameObject.Find("MouseCursor");
     }
 
     // Update is called once per frame
@@ -31,9 +31,19 @@ public class Aimer : MonoBehaviour {
     private void FixedUpdate()
     {
         // Gets mouse position and turns the object to it.
-        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
-        transform.rotation = rot;
-        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+        //var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
+        //transform.rotation = rot;
+        //transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+
+        ///LESS COMPLICATED VERSION// Keeping the above code as it gives sloppy rotation, which I might use for AI.
+        // convert mouse position into world coordinates
+        Vector2 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // get direction you want to point at
+        Vector2 direction = (mouseScreenPosition - (Vector2)transform.position).normalized;
+
+        // set vector of transform directly
+        transform.up = direction;
     }
 }
