ec5c211fd50debbef33f66753aae5ce4f6389f93
Avery Pratt <apratt91@gmail.com>
2019-02-08 21:51:26 -0800

    properly switched between in-game and menu cursor

--- Assets/Scripts/PlayerScripts/CursorScript.cs
+++ Assets/Scripts/PlayerScripts/CursorScript.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CursorScript : MonoBehaviour {
+public class CursorScript : MonoBehaviour
+{
+    public PauseMenu PauseMenu;
+    public SpriteRenderer InGameCursor;
 
 	// Use this for initialization
 	void Start ()
@@ -13,7 +16,19 @@ public class CursorScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+        SetCorrectCursor();
+
         var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = new Vector3 (mousePosition.x,mousePosition.y,0);
+    }
+
+    void SetCorrectCursor()
+    {
+        if (PauseMenu.GameIsPaused ^ Cursor.visible)
+        {
+            Cursor.visible = !Cursor.visible;
+        }
+
+        InGameCursor.gameObject.SetActive(!Cursor.visible);
     }
 }
