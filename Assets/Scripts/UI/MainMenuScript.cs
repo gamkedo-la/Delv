e7ebb7420167a78812d01f42286dc61f3a334995
Terrence <t.w.mcdonnell10@gmail.com>
2019-02-11 11:12:25 -0700

    Add change to MainMenuScript to disable dialogue box so other options aren't blocked

--- Assets/Scripts/UI/MainMenuScript.cs
+++ Assets/Scripts/UI/MainMenuScript.cs
@@ -7,13 +7,20 @@ public class MainMenuScript : MonoBehaviour {
 
     [SerializeField]
     private GameManagerScript GM;
+    private Scene currentScene;
+    public  GameObject dialogueBox;
 
 	// Use this for initialization
 	void Start ()
     {
         GameObject GMGO = GameObject.Find("GameManager");
         GM = GMGO.GetComponent<GameManagerScript>();
-	}
+        currentScene = SceneManager.GetActiveScene();
+        if (currentScene.name == "MainMenu")
+        {
+            dialogueBox.SetActive(false);
+        }
+    }
 	
     public void AIBot(bool isOn)
     {
@@ -26,12 +33,14 @@ public class MainMenuScript : MonoBehaviour {
         GM.PlayerCount = 1;
         GM.InitializeGame();
         SceneManager.LoadScene(1);
+        dialogueBox.SetActive(true);
     }
     public void Multiplayer()
     {
         GM.PlayerCount = 2;
         GM.InitializeGame();
         SceneManager.LoadScene(1);
+        dialogueBox.SetActive(true);
     }
 
     public void Settings()
