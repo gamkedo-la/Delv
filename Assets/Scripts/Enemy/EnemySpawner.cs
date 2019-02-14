935f136cdbb7d8d37d60786a191e664bad86ff2e
Trent <Xmedia101@yahoo.com>
2019-02-14 00:43:08 -0500

    Revert "Trees, Eye spawner fix, knockback reining in"
    This reverts commit 89d71fb1826649da84e686913817036642e619ca.

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -7,7 +7,6 @@ public class EnemySpawner : MonoBehaviour
     public GameObject EnemyPrefab;
     public Animator Animator;
     public EnemyManager spawner;
-    public GameObject SpawnerParticle;
     public Transform Eye;
 
     // Start is called before the first frame update
@@ -19,27 +18,15 @@ public class EnemySpawner : MonoBehaviour
 
     void SpawnEnemy()
     {
-
         GameObject EnemyInstance = Instantiate(EnemyPrefab, Eye.position, Eye.rotation);
         EnemyHealth EH = EnemyInstance.GetComponent<EnemyHealth>();
         EH.spawner = spawner;
-        StartCoroutine(Die());
-        
+        Die();
     }
-
-    void SpawnParticle()
+    void Die()
     {
-        if (GameManagerScript.instance.ParticleIntensity >= 2)
-        {
-            Instantiate(SpawnerParticle, transform.position, transform.rotation);
-        }
-    }
-
-    IEnumerator Die()
-    {
-        yield return new WaitForSeconds(.1f);
+        new WaitForSeconds(1);
         Destroy(gameObject);
     }
-
 
 }
