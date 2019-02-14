ed3462a637ead3d3dd7fa92a9be696e9fc559118
Terrence <t.w.mcdonnell10@gmail.com>
2019-02-14 00:32:12 -0700

    Add AI Follow Script

--- Assets/Scripts/CompanionAI/AICompanion.cs
+++ Assets/Scripts/CompanionAI/AICompanion.cs
@@ -8,84 +8,120 @@ public class AICompanion : MonoBehaviour
     public float hortDistance;
     public float vertNow;
     public float hortNow;
+
     public float aimCursorX;
     public float aimCursorY;
+
+    public float distanceBetween;
+    public int distanceToBeginFollow = 4;
 
     public PlayerController AIController;
     public GameObject BotGO;
 
     public GameObject PlayerGO;
-    private Collider playerCollider;
-    public bool testing;
+
+    public bool following;
 
     private Transform TargetPos;
 
     // Start is called before the first frame update
     public void Awake()
     {
-        playerCollider = PlayerGO.GetComponent<Collider>();
+        StartCoroutine(DistanceCheck());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (testing)
+        aimCursorX = Mathf.Cos(Time.timeSinceLevelLoad);
+        aimCursorY = Mathf.Sin(Time.timeSinceLevelLoad);
+        distanceBetween = Vector2.Distance(PlayerGO.transform.position, BotGO.transform.position);
+        if (Mathf.Abs(distanceBetween) > distanceToBeginFollow)
         {
-            hortDistance = PlayerGO.transform.position.x - BotGO.transform.position.x;
-            vertDistance = PlayerGO.transform.position.y - BotGO.transform.position.y;
-            hortNow = Mathf.Clamp(hortDistance, -1, 1);
-            vertNow = Mathf.Clamp(vertDistance, -1, 1);
+            StartCoroutine(DistanceCheck());
+        }
+        else
+        {
+            StopCoroutine(DistanceCheck());
+            following = false;
+        }
+
+        if (following)
+        {
+            FollowPlayer();
             return;
         }
         hortNow = 0.0f;
         vertNow = 0.0f;
-        aimCursorX = Mathf.Cos(Time.timeSinceLevelLoad);
-        aimCursorY = Mathf.Sin(Time.timeSinceLevelLoad);
     }
 
     void ShootAtTarget()
     {
-        // do nothing
+        // TODO
     }
 
     public void FollowPlayer()
     {
-        //transform.position = Vector3.MoveTowards(transform.position, TargetPos.position, AIController.speed);
+        hortDistance = PlayerGO.transform.position.x - BotGO.transform.position.x;
+        vertDistance = PlayerGO.transform.position.y - BotGO.transform.position.y;
+        hortNow = Mathf.Clamp(hortDistance, -1, 1);
+        vertNow = Mathf.Clamp(vertDistance, -1, 1);
+        return;
+    }
+
+    IEnumerator DistanceCheck()
+    {
+        float timeToWait = DiceRoll();
+        timeToWait = timeToWait/100;
+        yield return new WaitForSeconds(timeToWait);
+        Debug.Log("This is from the coroutine");
+        following = true;
+        yield break;
     }
 
     public float VertAxisNow()
     {
-        //OnTriggerEnter(playerCollider);
         return vertNow;
     }
 
     public float HortAxisNow()
     {
-        //OnTriggerEnter(playerCollider);
         return hortNow;
+    }
+
+    public void CursorAim()
+    {
+        // TODO
     }
 
     public float AimCursorX()
     {
         return aimCursorX;
     }
+
     public float AimCursorY()
     {
-        //OnTriggerEnter(playerCollider);
         return aimCursorY;
     }
 
-    public void CursorAim()
-    {
+    //void OnTriggerEnter(Collider other)
+    //{
 
-    }
+    //    if (other.tag == "Player")
+    //    {
+    //        following = false;
+    //        Debug.Log("Close to player - don't move");
+    //    }
+    //}
 
-    public void OnTriggerEnter(Collider collider2D)
-    {
-        Debug.Log("Close to player - don't move");
-        vertNow = 0.0f;
-        hortNow = 0.0f;
-    }
+    //void OnTriggerExit(Collider other)
+    //{
+    //    if (other.tag == "Player") 
+    //    {
+    //        following = true;
+    //        Debug.Log("Player moving - follow player");
+    //    }
+    //}
 
     public int DiceRoll()
     {
