8a4c263bb2c0322dd5c6b933c2c7f2cabe89a25f
Sdonai <silentstrum@gmail.com>
2019-03-14 05:27:30 -0700

    start implament slime anims

---
+++ Assets/Scripts/Enemy/AI/TelegraphAlert.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TelegraphAlert : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public MonoBehaviour TargetBehavior = null;
+    Animator m_Animator;
+    AnimatorStateInfo animStateInfo;
+    void Start()
+    {       
+       m_Animator = GetComponent<Animator>();
+       
+    }
+    void OnEnable()
+    {
+    	TargetBehavior.enabled = false; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	animStateInfo = m_Animator.GetCurrentAnimatorStateInfo(0);
+        if(!animStateInfo.IsName("Telegraph-Alert")){
+        	Debug.Log("has exited alert telegraph");
+        	TargetBehavior.enabled = true; 
+        	enabled = false;
+        } else {
+        	Debug.Log("Still in telegraph state and animation: " +Animator.StringToHash("Base.Telegraph-Alert") + " / "+ Animator.StringToHash("Telegraph-Alert") + " / "+ animStateInfo.nameHash);
+        }
+    }
+}
