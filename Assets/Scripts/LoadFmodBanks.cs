e6b96341fafac5bbbcca01267ba03296d2f8d3d0
Gnommy <th.brandoli@gmail.com>
2019-03-26 11:13:46 +0100

    implemented fmod + add a script to load the banks

---
+++ Assets/Scripts/LoadFmodBanks.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadFmodBanks : MonoBehaviour
+{
+    [FMODUnity.BankRef]
+    public List<string> banks;
+    bool HasBankLoaded = false;
+
+    private void Awake()
+    {
+        foreach (string b in banks)
+        {
+            FMODUnity.RuntimeManager.LoadBank(b, true);
+            Debug.Log("Loaded bank " + b);
+        }
+    }
+
+    void Update()
+    {
+        if(!HasBankLoaded)
+        {
+            if (FMODUnity.RuntimeManager.HasBankLoaded("Master Bank"))
+            {
+                Debug.Log("Master Bank Loaded");
+                HasBankLoaded = true;
+            }
+        }
+    }
+}
