aac7de3c17654db03f15afc607f7b574bcb62364
Caspar Dunant <spadxiii@gmail.com>
2019-03-30 22:19:19 +0100

    Add falling rocks hazard, prefab shown in test hazards scene

---
+++ Assets/Scripts/FallingRock.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingRock : MonoBehaviour
+{
+    [SerializeField] Transform rock;
+    [SerializeField] Transform shadow;
+    [SerializeField] float shadowSizePercentage = 0.6f;
+    [SerializeField] float fallSpeed = 5f;
+
+    Vector3 shadowScale;
+
+    float maxDistance;
+    public bool fallen = false;
+    Vector3 orgPos;
+
+    void Start()
+    {
+        shadowScale = shadow.localScale;
+        shadow.localScale = shadowScale * shadowSizePercentage;
+
+        maxDistance = Vector3.Distance(rock.position, shadow.position);
+        orgPos = rock.position;
+    }
+
+    void Update()
+    {
+        if (fallen) {
+            return;
+        }
+
+        float distance = Vector3.Distance(rock.position, shadow.position);
+        shadow.localScale = shadowScale * Mathf.Lerp(shadowSizePercentage, 1.0f, (maxDistance - distance) / maxDistance);
+
+        rock.position = Vector3.MoveTowards(rock.position, shadow.position, fallSpeed * Time.deltaTime);
+
+        fallSpeed *= 1.01f;
+
+        fallen = (distance < 0.001f);
+        if (fallen) {
+            // @todo do damage
+            Destroy(gameObject, 2f);
+        }
+    }
+}
---
+++ Assets/Scripts/FallingRocksHazard.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingRocksHazard : MonoBehaviour
+{
+	[Range(2, 10)]
+	[SerializeField] int minTimeout = 2;
+	[Range(10, 20)]
+	[SerializeField] int maxTimeout = 15;
+	[SerializeField] float radius = 2f;
+	[SerializeField] GameObject fallingRockPrefab;
+
+	void Start()
+	{
+		StartCoroutine(spawnRocks());
+	}
+
+	IEnumerator spawnRocks()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(Random.Range(minTimeout, maxTimeout));
+
+			Vector3 position = Random.insideUnitCircle * radius;
+			Instantiate(fallingRockPrefab, position, Quaternion.Euler(0f, 0f, 0f));
+		}
+	}
+
+	void OnDrawGizmosSelected()
+	{
+		UnityEditor.Handles.color = Color.green;
+		UnityEditor.Handles.DrawWireDisc(transform.position, Vector3.forward, radius);
+	}
+}
