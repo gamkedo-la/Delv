25f637854de59e343fe351014f5324745377507a
bilalCh213 <boyz213@live.com>
2019-03-30 19:36:10 +0500

    Added more Tiles to Interior Tileset and Added Shop (entry/exit only, not function)

---
+++ Assets/Scripts/TriggerToggle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerToggle : MonoBehaviour
+{
+	public GameObject[] disable;
+	public GameObject[] enable;
+	
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		for (int i = 0; i < disable.Length; i++)
+			disable[i].SetActive(false);
+
+		for (int i = 0; i < enable.Length; i++)
+			enable[i].SetActive(true);
+	}
+}
