3277dd1f6b3d784f1caf7b59042101989ef0fcfc
bilalCh213 <boyz213@live.com>
2019-04-01 20:37:15 +0500

    Fixed the Bug: Player's fireball triggering house entry/exit

--- Assets/Scripts/TriggerToggle.cs
+++ Assets/Scripts/TriggerToggle.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class TriggerToggle : MonoBehaviour
 {
+	public string colliderTag = "Player";
 	public GameObject[] disable;
 	public GameObject[] enable;
 	
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
-		for (int i = 0; i < disable.Length; i++)
-			disable[i].SetActive(false);
+		if (collision.gameObject.tag == colliderTag)
+		{
+			for (int i = 0; i < disable.Length; i++)
+				disable[i].SetActive(false);
 
-		for (int i = 0; i < enable.Length; i++)
-			enable[i].SetActive(true);
+			for (int i = 0; i < enable.Length; i++)
+				enable[i].SetActive(true);
+		}
 	}
 }
