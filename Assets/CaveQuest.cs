0fa749782e6417c5e7fec95883358932da779271
Trent <Xmedia101@yahoo.com>
2019-04-01 20:06:29 -0400

    Cave Quest Stuff

---
+++ Assets/CaveQuest.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaveQuest : MonoBehaviour
+{
+    public GameObject Gate;
+    public GameObject ObeliskBlue;
+    public NPCTalkTrigger BlueTalk;
+    public GameObject ObeliskGreen;
+    public NPCTalkTrigger GreenTalk;
+    [Space]
+    public bool BlueFlipped;
+    public bool GreenFlipped;
+    [Space]
+    public bool BlueGate;
+    public bool GreenGate;
+    [Space]
+    private bool QuestComplete;
+
+    void FlipBlue()
+    {
+        BlueFlipped = true;
+    }
+    void FlipGreen()
+    {
+        GreenFlipped = true;
+    }
+
+    void GateBlue()
+    {
+        BlueGate = true;
+        if (GreenGate)
+        {
+            Complete();
+        }
+
+    }
+    void GateGreen()
+    {
+        GreenGate = true;
+        if (BlueGate)
+        {
+            Complete();
+        }
+    }
+
+    void Complete()
+    {
+        QuestComplete = true;
+        //move the gate.
+    }
+
+}
---
+++ Assets/SendMessageOnActivate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendMessageOnActivate : MonoBehaviour
+{
+
+    public GameObject Target;
+    public string Message;
+    [Space]
+    public bool Activated;
+
+    void Activate()
+    {
+        if (!Activated)
+        {
+            Target.SendMessage(Message);
+            Activated = true;
+
+        }
+    }
+}
