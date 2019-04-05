fff43cb72abf9ff767bff857a0d162ebd52904df
bilalCh213 <boyz213@live.com>
2019-04-06 01:24:12 +0500

    Added Gameover State

---
+++ Assets/DelayActivation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayActivation : MonoBehaviour
+{
+	public float delay = 1f;
+	public GameObject[] delayedObjects;
+
+	private float _delay = 0f;
+	private bool done = false;
+
+	void Start()
+	{
+		_delay = delay;
+		done = false;
+	}
+
+	void OnEnable()
+    {
+		_delay = delay;
+		done = false;
+    }
+	
+    void Update()
+    {
+		if (!done)
+		{
+			if (_delay <= 0f)
+			{
+				foreach (var o in delayedObjects)
+					o.SetActive(true);
+				done = true;
+			}
+			else
+				_delay -= Time.deltaTime;
+		}
+    }
+}
---
+++ Assets/GameOverConditionScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverConditionScript : MonoBehaviour
+{
+	public GameObject[] players;
+	public GameObject gameOverUX;
+
+    void Start()
+    {
+    }
+	
+    void Update()
+    {
+		if ((players[0].activeSelf == false || players[0].GetComponent<PlayerController>().Health <= 0f)
+		&& (players[1].activeSelf == false || players[1].GetComponent<PlayerController>().Health <= 0f))
+		{
+			gameOverUX.SetActive(true);
+		}
+    }
+}
