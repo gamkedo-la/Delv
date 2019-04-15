bc8465081a7f1f2910ac8d0389916371d9e7b417
Tylor Allison <tylor@foobox.com>
2019-04-15 09:42:18 -0500

    Revert "Add Music Controller script + implement forest ambience"
    This reverts commit b0b0a6f0d7bceb8e4ad95e6e9ab45d009d6fe818.

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -422,7 +422,7 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    // Control FMOD the footsteps parameter
+    // Control the footsteps parameter
     private void OnTriggerEnter2D(Collider2D collision)
     {
         switch (collision.gameObject.tag)
