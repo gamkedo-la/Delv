cbdbdf5b7a150a3332f83b830c7ce217de0a3f0a
bilalCh213 <boyz213@live.com>
2019-04-25 00:24:36 +0500

    Bug Fix: Dialogue Panel shows up for a moment when Starter Village scene loads.

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -174,7 +174,7 @@ public class PlayerController : MonoBehaviour
         _rb = GetComponent<Rigidbody2D>();
         SR = Alive.GetComponent<SpriteRenderer>();
         SP = GameObject.FindGameObjectWithTag("StartZone");
-        TimeManager = GameManagerScript.instance.GetComponent<TimeManager>();
+        //TimeManager = GameManagerScript.instance.GetComponent<TimeManager>();
         MainCam = Camera.main;
         HealthBar = healthdirector.HealthBar;
         EnergyBar = healthdirector.EnergyBar;
